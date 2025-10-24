2b4a9e08a39e5e369de0ebde3dda6446d2cda184
Hassan <104513483+hassan-980@users.noreply.github.com>
2025-10-24 15:37:28 +0530

    Time: 556 ms (22.61%), Space: 254.9 MB (16.08%) - LeetHub

---
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    bool balanced(int num){
+        vector<int> count(10);
+
+        while( num > 0){
+            int digit = num%10;
+            count[digit]++;
+            num = num/10;
+        }
+
+
+        for(int i=0; i<10;i++){
+            if( count[i] != 0 && count[i] != i){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    int nextBeautifulNumber(int n) {
+
+        for(int num = n+1; num <= 1224444 ; num++){
+            if(balanced(num)){
+                return num;
+            }
+        }
+
+        return -1;
+        
+    }
+};
