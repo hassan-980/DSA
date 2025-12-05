e2c412ea88b054e42c0d70bf4bb1e87cdfb465f4
Hassan <104513483+hassan-980@users.noreply.github.com>
2025-12-05 17:22:55 +0530

    Time: 0 ms (100%), Space: 22.3 MB (96.78%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -4,10 +4,7 @@ public:
 
         int left =0, right=0,totalSum=0, count=0;
         int n = nums.size();
-        for(int i =0; i<n; i++){
-            totalSum+= nums[i];
-        }
-        
+        totalSum = accumulate(begin(nums),end(nums),0);
         for(int i=0;i<=n-2;i++){
             left+= nums[i];
             right = totalSum - left;
