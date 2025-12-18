ebd10210201c75e7e891a3ba1283b50b5e2e2978
Hassan <104513483+hassan-980@users.noreply.github.com>
2025-12-18 20:54:48 +0530

    Time: 6 ms (85.9%), Space: 316.1 MB (52.22%) - LeetHub

---
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
+        typedef long long ll;
+        int n = prices.size();
+
+        ll actualProfit = 0;
+        vector<ll> profit(n);
+
+        for( int i = 0; i < n; i++ ){
+            profit[i] = (ll)strategy[i] * prices[i];
+            actualProfit += profit[i];
+        }
+
+        ll originalWindowProfit = 0;
+        ll modifiedWindowProfit = 0;
+        ll maxGain = 0;
+
+        int i = 0, j = 0;
+
+        while( j < n ){
+
+            originalWindowProfit += profit[j];
+
+            // Second half of the window started
+            if( j - i + 1 > k/2 ){
+                modifiedWindowProfit += prices[j];
+            }
+
+            if( j - i + 1 > k ){
+                originalWindowProfit -= profit[i];
+                modifiedWindowProfit -= prices[ i + k/2 ];
+                i++;
+            }
+
+            if( j - i + 1 == k){
+                maxGain = max( maxGain , modifiedWindowProfit - originalWindowProfit );
+            }
+
+            j++;
+
+        }
+
+        return actualProfit + maxGain;
+
+        
+    }
+};
