3e8a1ff5d13d0e01036923246cb8b54e0764a544
Hassan <104513483+hassan-980@users.noreply.github.com>
2026-02-17 22:25:51 +0530

    Time: 0 ms (100%), Space: 8.4 MB (91.37%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -19,37 +19,5 @@ public:
         }
     return true;
 
-
-
-
-
-
-
-
-
-
-
-
-        // int temp,digit;
-        // long long rev=0;
-        // if( x < 0 ){
-        //     return false;
-        // }
-        // temp = x;
-
-        // while( temp>0 ){
-        //     digit = temp % 10;
-        //     rev = rev * 10 + digit;
-        //     temp /= 10;
-
-        // }
-
-        // if( rev == x ){
-        //     return true;
-        // }else{
-        //     return false;
-        // }
-
-
     }
 };
