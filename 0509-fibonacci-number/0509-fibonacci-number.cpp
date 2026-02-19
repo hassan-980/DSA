4b109e7f2e0a681583cd8271f560cc21a75c54a0
Hassan <104513483+hassan-980@users.noreply.github.com>
2026-02-19 16:47:02 +0530

    Time: 13 ms (31.8%), Space: 7.8 MB (36.3%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    int fib(int n) {
+    int fib(int N) {
+           if (N <= 1) {
+        return N;
+    }
+    // Recursive case: sum of (N-1)th and (N-2)th Fibonacci numbers
+    int last = fib(N - 1);   // (N-1)th Fibonacci
+    int slast = fib(N - 2);  // (N-2)th Fibonacci
 
-        if(n <= 1)
-            return n;
-
-        int secLast = 0;
-        int last = 1;
-        for(int i = 2; i <= n; i++){
-            int curr = last + secLast;
-            secLast = last;
-            last = curr;
-        }
-
-        return last;
+    return last + slast;
     }
 };
 
