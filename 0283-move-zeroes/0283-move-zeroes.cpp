875de433679e185e82d2bf45e89de684720f9566
Hassan <104513483+hassan-980@users.noreply.github.com>
2026-02-21 01:24:50 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.56%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        // Pointer to the first zero
+        int j = -1;
+
+        // Find the first zero
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // If no zero found, return
+        if (j == -1) return;
+
+        // Start from the next index of first zero
+        for (int i = j + 1; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
