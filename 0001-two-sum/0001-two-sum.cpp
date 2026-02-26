39954542bc1b338de4f413d76b904d95d5716209
Hassan <104513483+hassan-980@users.noreply.github.com>
2026-02-27 01:14:08 +0530

    Time: 1 ms (77.22%), Space: 14.9 MB (18.4%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& arr, int target) {
-        // unordered_map<int, int> mp;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
         
-        // for(int i = 0; i<nums.size(); i++) {
-        //     int remaining = target -  nums[i];
-        //     if(mp.count(remaining))
-        //         return {mp[target-nums[i]], i};
-        //     mp[nums[i]] = i;
+        for(int i = 0; i<nums.size(); i++) {
+            int remaining = target -  nums[i];
+            if(mp.count(remaining))
+                return {mp[target-nums[i]], i};
+            mp[nums[i]] = i;
+        }
+        
+        return {};
+
+
+        // int n = arr.size();
+        // vector<pair<int, int>> numsWithIndex;
+        // for (int i = 0; i < n; i++) {
+        //     numsWithIndex.push_back({arr[i], i});
         // }
-        
-        // return {};
+        // sort(numsWithIndex.begin(), numsWithIndex.end());
 
-
-        int n = arr.size();
-        vector<pair<int, int>> numsWithIndex;
-        for (int i = 0; i < n; i++) {
-            numsWithIndex.push_back({arr[i], i});
-        }
-        sort(numsWithIndex.begin(), numsWithIndex.end());
-
-        int left = 0, right = n - 1;
-        while (left < right) {
-            int sum = numsWithIndex[left].first + numsWithIndex[right].first;
-            if (sum == target) {
-                // Return original indices
-                return {numsWithIndex[left].second, numsWithIndex[right].second};
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-        return {};  // No pair found
+        // int left = 0, right = n - 1;
+        // while (left < right) {
+        //     int sum = numsWithIndex[left].first + numsWithIndex[right].first;
+        //     if (sum == target) {
+        //         // Return original indices
+        //         return {numsWithIndex[left].second, numsWithIndex[right].second};
+        //     } else if (sum < target) {
+        //         left++;
+        //     } else {
+        //         right--;
+        //     }
+        // }
+        // return {};  // No pair found
     }
 };
